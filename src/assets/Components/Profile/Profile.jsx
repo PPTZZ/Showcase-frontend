27ecce81f68e336df58f69052a777047c47f4efd
unknown <poputa.alex@gmail.com>
2023-12-04 14:43:07 +0200

    paused profile to test

--- src/assets/Components/Profile/Profile.jsx
+++ src/assets/Components/Profile/Profile.jsx
@@ -5,36 +5,49 @@ const Profile = () =>{
     return(
         <>
             <nav>
-                <img src='/Logo.svg' alt='Logo'/>
-                <img src='/bell icon.svg' alt='subscriibe bell'/>
-                <Link to='/' className='reactLink'>
-                    <p>Log Out</p>
+                <img src='/Logo.svg' alt='Logo' className='logo'/>
+                <img src='/bell icon.svg' alt='subscriibe bell' className='subscribe'/>
+                <Link to='/' className='reactLink logOut'>
+                    <p className='logOut'>Log Out</p>
                 </Link>
             </nav>
             <div>
-                <img src='/keyboard.svg'/>
-                <div className='sidebar'>
-                    <img src='/Profile Picture.svg'/>
-                    <p>{"name"}</p>
-                    <p>{""}</p>
-                    <p>{"email"}</p>
-                    <p>{"locaton"}</p>
-                    <button><img src="/edit.svg"/>Edit Profile</button>
-                    <p>Member since: {"date"}</p>
+                <div className='keyboardContainer'>
+                    <img src='/keyboard.svg' width={'100%'}/>
                 </div>
-                <div className='display'>
-                    <Link to='/projects' className='reactLink'>
-                        <p>Projects</p>
-                    </Link>
-                    <Link to='/drafts' className='ractLink'>
-                        <p>Drafts</p>
-                    </Link>
-                </div>
-                <div className='canvas'>
-                    <Link to='/upload' className='reactLink'>
-                        <button><img src='/add.svg'/>New project </button>
-                    </Link>
-
+                <div className='bodyContainer'>
+                    <div className='sidebar'>
+                        <img className='profPic' src='/Profile Picture.svg'/>
+                        <p className='name'>{"Jackson Brown"}</p>
+                        <p className='job'>{"Web Developer"}</p>
+                        <a href='mailto:fakemail@yopmail.com'>
+                            <div className='email'>
+                                <img src='/mail.svg' />
+                                <p className='marginless'>{"email"}</p>
+                            </div>
+                        </a>
+                        <div className='loc'>
+                            <img src='/location_on.svg'/>
+                            <p className='marginless'>{"locaton"}</p>
+                        </div>
+                        <div className='lowerPage'>
+                            <button className='profileBtn'><img src="/edit.svg"/>Edit Profile</button>
+                            <p className='member'>Member since: {"date"}</p>
+                        </div>
+                    </div>
+                    <div className='display'>
+                        <Link to='/projects' className='reactLink'>
+                            <p>Projects</p>
+                        </Link>
+                        <Link to='/drafts' className='reactLink'>
+                            <p>Drafts({'0'})</p>
+                        </Link>
+                    </div>
+                    <div className='canvas'>
+                        <Link to='/upload' className='reactLink'>
+                            <button className='uploadBtn'><img src='/add.svg'/>New project </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
